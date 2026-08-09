5268cea4c5352ae87c778e016d5072ae6033c4ea
Salman472 <mds619820@gmail.com>
2026-08-09 18:40:00 +0600

    refactor(config): update environment variables and router imports

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const config={
     databaseUrl: process.env.DATABASE_URL || '',
     port: process.env.PORT || 3000,
+    jwt_access_token_secret: process.env.JWT_ACCESS_SECRET || '',
+    jwt_refresh_token_secret: process.env.JWT_REFRESH_SECRET || '',
     
 }
 
--- src/app/routers/index.ts
+++ src/app/routers/index.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
-import { AuthRouter } from "../modules/auth/auth.validation.js";
+import { AuthRouter } from "../modules/auth/auth.route.js";
+
 
 const router : Router = Router();
 
 router.use('auth',AuthRouter);
+
+export default router;
