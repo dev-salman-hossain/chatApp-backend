30b986ec95f4ef63c0620c3ae3044446123bbce8
Salman472 <mds619820@gmail.com>
2026-08-09 12:05:00 +0600

    fix(app): fix express rate limit typo and import router

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import experssRateLimit from 'express-rate-limit';
+import expressRateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import hpp from 'hpp'; 
-
-
+import router from './app/routers/index.js';
 
 const app : Application = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(experssRateLimit());
+app.use(expressRateLimit());
 app.use(helmet());
 app.use(hpp());
 
+app.use('/api/v1', router);
 
 app.get('/', (_req, res) => {
-    res.send('Hello World');
+    res.send('Server is running');
 });
 
 export default app;
